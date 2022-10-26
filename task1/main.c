47638dc0533fc6c9d36da55a5e36cfca5770ad0d
lsyyy <3128672783@qq.com>
2022-10-26 20:21:11 +0800

    GEEK work

--- task1/main.c
+++ task1/main.c
@@ -1,5 +1,22 @@
 #include<stdio.h>
- int main(){
+int main()
+{double y =0 ;
+int d =1;
+double a=1,b,x;
+printf("a =");
+printf("x     f   \n");
+b =0.999*a;
+y=b+1.0;
+for(x=0.001;y-b>0.000001;d+=1)
+{
+ x=0.001*d;
+ y=b;
+ printf("%f   %f\n",x,y);
+ a=y;
+ b=0.999*a;
+}
+return 0;
+}
   
    
   
@@ -9,8 +26,6 @@
   
   
   
- return 0;
- }
  
  
 
