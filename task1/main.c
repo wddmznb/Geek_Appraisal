51079efed9afa48bbc89c73ecb9d9926c006048a
lsyyy <3128672783@qq.com>
2022-10-26 19:26:55 +0800

    add work for GEEK

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+ int main(){
+  
+   
+  
+  
+  
+  
+  
+  
+  
+ return 0;
+ }
+ 
+ 
 
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
-	return 0;
-}
+ 
+
+
+ 
+
+ 
 
